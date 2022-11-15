5f6f325691984ad4313547384d55e57568576b6b
woongsnote <mjw92@kakao.com>
2022-11-15 16:08:34 +0900

    feat: pwa 구현

--- components/meta.tsx
+++ components/meta.tsx
@@ -3,9 +3,17 @@ import Head from "next/head";
 const Meta = () => (
   <Head>
     <title>WoongsNote</title>
-    <meta name="description" content="NextJs Blog by Woongsnote" />
-    <link rel="icon" href="/favicon.ico" />
     <meta charSet="utf-8" />
+    <meta name="application-name" content="Woongsnote" />
+    <meta name="description" content="NextJs Blog by Woongsnote" />
+    <meta name="theme-color" content="#4a90e2" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover"
+    />
+    <link rel="apple-touch-icon" href="/icons/icon-512x512.png" />
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="manifest" href="/manifest.json" />
   </Head>
 );
 
--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -10,6 +16,6 @@ const nextConfig = {
       },
     ],
   },
-};
+});
 
 module.exports = nextConfig;
