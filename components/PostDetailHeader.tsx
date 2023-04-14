24784face8649295f46f5ad09a0b75de0cee758e
woongsnote <woongsnote@gmail.com>
2023-04-14 18:01:50 +0900

    fix: 상세페이지 레이아웃 조정

--- components/PostDetailHeader.tsx
+++ components/PostDetailHeader.tsx
@@ -2,7 +2,7 @@ import { Post } from "@/.contentlayer/generated";
 
 const PostDetailHeader = ({ post }: { post: Post }) => {
   return (
-    <section className="py-2 mb-8">
+    <section className="py-2 mb-8 mx-24">
       <h1 className="text-5xl font-bold mb-1">{post.title}</h1>
       <div className="flex justify-between items-center">
         <h2 className="text-md md:text-lg text-gray-700 dark:text-gray-300">
--- components/ProjectDetailHeader.tsx
+++ components/ProjectDetailHeader.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 
 const ProjectDetailHeader = ({ project }: { project: Project }) => {
   return (
-    <div className="py-2 mb-8">
+    <div className="py-2 mb- mx-24">
       <h1 className="text-5xl font-bold mb-1">{project.title}</h1>
       <h2 className="mb-2 text-md md:text-lg text-gray-700 dark:text-gray-300">
         {project.description}
       </h2>
-      <div className="h-72 mx-auto w-3/4 relative rounded-xl items-center block justify-center">
+      <div className="max-h-72 h-auto mx-auto w-full relative rounded-xl items-center block justify-center">
         <Image
           src={project.coverImage}
           alt="coverImage"
           priority
-          width={360}
-          height={280}
-          className="rounded-md w-auto h-auto object-contain mx-auto"
+          width={300}
+          height={200}
+          className="rounded-md w-auto h-auto object-contain mx-auto my-6"
         />
       </div>
     </div>
