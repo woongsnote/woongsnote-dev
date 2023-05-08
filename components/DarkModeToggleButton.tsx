06f45c26828a5834d08ec6b9b5976e6999cda8de
woongsnote <woongsnote@gmail.com>
2023-05-08 18:01:50 +0900

    fix: 다크모드 토글버튼 네이밍 개선 및 스타일 변경

--- components/DarkModeToggleButton.tsx
+++ components/DarkModeToggleButton.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { BsSunFill, BsMoonFill } from "react-icons/bs";
 
-const ThemeSwitch = () => {
+const DarkModeToggleButton = () => {
   const [mounted, setMounted] = useState(false);
   const { systemTheme, theme, setTheme } = useTheme();
 
@@ -15,24 +15,28 @@ const ThemeSwitch = () => {
   const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
-    <div className="sm:ml-2 flex justify-center z-50">
+    // <div className="flex justify-center z-50">
+    <>
       {currentTheme === "dark" ? (
         <button
           aria-label="dark mode"
-          className="p-1 hover:text-yellow-300 rounded-full bg-gray-700"
-          onClick={() => setTheme("light")}>
+          className="m-1 hover:text-yellow-300 rounded-full bg-gray-700"
+          onClick={() => setTheme("light")}
+        >
           <BsSunFill size={28} />
         </button>
       ) : (
         <button
           aria-label="light mode"
-          className="p-1 hover:text-yellow-700 rounded-full shadow-lg"
-          onClick={() => setTheme("dark")}>
+          className="m-1 hover:text-yellow-700 rounded-full"
+          onClick={() => setTheme("dark")}
+        >
           <BsMoonFill size={28} />
         </button>
       )}
-    </div>
+    </>
+    // </div>
   );
 };
 
-export default ThemeSwitch;
+export default DarkModeToggleButton;
