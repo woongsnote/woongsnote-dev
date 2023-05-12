73f1aa701dbd423ac266d0f983cfbbd37c3e1785
woongsnote <woongsnote@gmail.com>
2023-05-12 10:18:31 +0900

    fix: github repo 이름 수정에 따른 댓글 컴포넌트 링크 수정

--- components/Utterance.tsx
+++ components/Utterance.tsx
@@ -11,7 +11,7 @@ const Utterance = () => {
         const scriptElement = document.createElement("script");
         scriptElement.src = "https://utteranc.es/client.js";
         scriptElement.async = true;
-        scriptElement.setAttribute("repo", "woongsnote/my-tech-blog");
+        scriptElement.setAttribute("repo", "woongsnote/woongsnote-dev");
         scriptElement.setAttribute("issue-term", "pathname");
         scriptElement.setAttribute("theme", "preferred-color-scheme");
         scriptElement.crossOrigin = "anonymous";
