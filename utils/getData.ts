42fdef1b06827098d22cfeb88504257cf975e4a7
woongsnote <woongsnote@gmail.com>
2023-05-23 09:28:17 +0900

    refactor: any type 제거

--- utils/getData.ts
+++ utils/getData.ts
@@ -1,8 +1,10 @@
 import { compareDesc } from "date-fns";
 
-export const getSortedDataList = (data: any[], maxNum?: number) => {
+export const getSortedDataList = <T extends { date: string }>(data: T[], maxNum?: number) => {
 
-  const sortedData = data.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+  const sortedData = data.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  );
 
   return maxNum ? sortedData.slice(0, maxNum) : sortedData;
 };
