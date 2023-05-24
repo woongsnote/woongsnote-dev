ed5de0d10e57c3378b482f1333c13604d6506b6e
woongsnote <woongsnote@gmail.com>
2023-05-24 14:06:20 +0900

    refactor: layout 조정

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import Banner from "./components/Banner";
-import Container from "./components/Container";
 import RecentPostListSection from "./components/RecentPostListSection";
 import RecentProjectListSection from "./components/RecentProjectListSection";
-
 
 export default function Home() {
 
   return (
-    <Container>
+    <>
       <Banner />
       <RecentPostListSection />
       <RecentProjectListSection />
-    </Container>
+    </>
   );
 }
