dfe469c894591f4d7d7a763f98e0ca132bdb98b4
woongsnote <woongsnote@gmail.com>
2023-06-16 18:02:37 +0900

    fix: mobile 레이아웃 수정

--- app/components/PostList.tsx
+++ app/components/PostList.tsx
@@ -3,12 +3,11 @@ import PostCard from "./PostCard";
 
 const PostList = ({ posts }: { posts: Post[] }) => {
   return (
-      <div className="grid grid-cols-2 gap-y-4 gap-x-6 xl:gap-x-8 pt-4">
-        {posts.map((post) => (
-          <PostCard key={post.slug} {...post} />
-        ))}
-      </div>
-  
+    <div className="grid md:grid-cols-2 gap-y-4 gap-x-6 xl:gap-x-8 pt-4">
+      {posts.map((post) => (
+        <PostCard key={post.slug} {...post} />
+      ))}
+    </div>
   );
 };
 export default PostList;
