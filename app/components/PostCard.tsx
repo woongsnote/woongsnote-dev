c6cacb445ed76875c708006beca1895154952f3c
woongsnote <woongsnote@gmail.com>
2023-06-30 16:32:20 +0900

    style: 날짜 형식 변경

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 import { Post } from 'contentlayer/generated';
-import { getMonth, getYear } from 'date-fns';
+import { getYear, format } from 'date-fns';
 
 const PostCard = (post: Post) => {
   return (
     <Link href={`/blog/${post.slug}`} className="group">
       <article className="py-3 px-2 hover:text-indigo-500 shadow-md rounded-lg dark:border-2 dark:border-gray-500 md:group-hover:scale-105 transition-transform duration-200 ease-out flex flex-row gap-4">
-        <div className="flex flex-col gap-1 items-center justify-center">
-          <span className='text-sm text-black'>{getYear(new Date(post.date))}</span>
-          <span className='text-black font-bold text-lg'>{getMonth(new Date(post.date)) + 1}</span>
+        <div className="flex flex-col gap-1 items-center justify-center text-gray-700 dark:text-gray-300">
+          <span className="text-sm">{getYear(new Date(post.date))}</span>
+          <span className="text-md">
+            {format(new Date(post.date), 'MMM dd')}
+          </span>
         </div>
-        <div>
+        <div className="flex justify-center flex-col">
           <h2 className="text-xl font-bold md:text-2xl">{post.title}</h2>
           <p className="text-sm text-gray-700 dark:text-gray-300">
             {post.description}
--- app/layouts/PostDetailHeader.tsx
+++ app/layouts/PostDetailHeader.tsx
@@ -1,14 +1,16 @@
-import { Post } from "contentlayer/generated";
-
+import { Post } from 'contentlayer/generated';
+import { format } from 'date-fns';
 const PostDetailHeader = ({ post }: { post: Post }) => {
   return (
-    <section className="py-2 mb-8 md:mx-24">
+    <section className="py-2 mb-8 md:mx-24 md:mt-2">
       <h1 className="text-3xl md:text-5xl font-bold mb-1">{post.title}</h1>
       <div className="flex flex-col md:flex-row md:justify-between md:mt-3">
         <h2 className="text-md md:text-lg text-gray-700 dark:text-gray-300 w-full md:w-fit">
           {post.description}
         </h2>
-        <time className="text-sm">{post.date}</time>
+        <time className="text-sm">
+          {format(new Date(post.date), 'yyyy년 MM월 dd일')}
+        </time>
       </div>
     </section>
   );
