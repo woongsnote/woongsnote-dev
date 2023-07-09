2ce91fd8d831c69d06fb2c6bf847028c16c22a24
woongsnote <woongsnote@gmail.com>
2023-07-09 10:03:05 +0900

    fix: og image naming 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: '/og-image.png',
+        url: '/opengraph-image.png',
         width: 1200,
         height: 630,
         alt: "Woong's Tech Blog",
