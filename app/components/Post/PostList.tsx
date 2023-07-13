dc74737ba5abe70b838c5321dc715c720944cbf8
woongsnote <woongsnote@gmail.com>
2023-07-13 13:39:55 +0900

    style: blog UI 수정
    - 한 줄에 하나씩 표시

--- app/components/Post/PostList.tsx
+++ app/components/Post/PostList.tsx
@@ -3,7 +3,7 @@ import PostCard from './PostCard';
 
 const PostList = ({ posts }: { posts: Post[] }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-6 xl:gap-x-8 pt-4">
+    <div className="grid grid-cols-1 gap-y-4 gap-x-6 xl:gap-x-8 pt-4">
       {posts.map((post) => (
         <PostCard key={post._id} {...post} />
       ))}
