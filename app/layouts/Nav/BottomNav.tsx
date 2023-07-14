8836388bdbe6bf7ff10bb32a763eca54b11ca410
woongsnote <woongsnote@gmail.com>
2023-07-14 10:57:20 +0900

    refactor: 불필요한 태그 제거

--- app/layouts/Nav/BottomNav.tsx
+++ app/layouts/Nav/BottomNav.tsx
@@ -6,28 +6,26 @@ import { Links } from './Links';
 const BottomNav = (): React.ReactElement => {
   const pathname = usePathname();
   return (
-    <>
-      <div className="sm:hidden">
-        <nav className="fixed bottom-0 w-full border-t">
-          <div className="mx-auto flex h-14 items-center justify-around bg-white dark:bg-black">
-            {Links.map(({ bottomLabel, href, icon }) => (
-              <Link
-                key={bottomLabel}
-                href={href}
-                className={`text-sm w-full h-full flex flex-col items-center justify-center ${
-                  pathname === href
-                    ? 'text-indigo-500 dark:text-indigo-400 '
-                    : 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50'
-                }`}
-              >
-                <span className="text-lg py-1">{icon}</span>
-                <span className="text-xs">{bottomLabel}</span>
-              </Link>
-            ))}
-          </div>
-        </nav>
-      </div>
-    </>
+    <div className="sm:hidden">
+      <nav className="fixed bottom-0 w-full border-t">
+        <div className="mx-auto flex h-14 items-center justify-around bg-white dark:bg-black">
+          {Links.map(({ bottomLabel, href, icon }) => (
+            <Link
+              key={bottomLabel}
+              href={href}
+              className={`text-sm w-full h-full flex flex-col items-center justify-center ${
+                pathname === href
+                  ? 'text-indigo-500 dark:text-indigo-400 '
+                  : 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50'
+              }`}
+            >
+              <span className="text-lg py-1">{icon}</span>
+              <span className="text-xs">{bottomLabel}</span>
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </div>
   );
 };
 export default BottomNav;
