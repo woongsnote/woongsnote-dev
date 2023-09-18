39694c8beaa72ffcaf9c004c6e2365f2fd2367b0
woongsnote <woongsnote@gmail.com>
2023-09-18 15:45:17 +0900

    fix: add aria-label to link

--- app/components/Header/GitHubLink.tsx
+++ app/components/Header/GitHubLink.tsx
@@ -4,7 +4,7 @@ const GITHUB_LINK = 'https://github.com/woongsnote/woongsnote-dev';
 
 export default function GitHubLink() {
   return (
-    <a href={GITHUB_LINK} className="m-1">
+    <a href={GITHUB_LINK} className="m-1" aria-label="move to github">
       <BsGithub size={30} />
     </a>
   );
