6fe7d863f9ae210a46752aeaf97908882e369324
woongsnote <woongsnote@gmail.com>
2023-11-02 17:40:52 +0900

    style: update article style
    - fix content color

--- app/ui/post/ReadingTimeText.tsx
+++ app/ui/post/ReadingTimeText.tsx
@@ -5,7 +5,7 @@ type ReadingTimeProps = {
 export const ReadingTimeText = ({ readingTime }: ReadingTimeProps) => {
   return (
     <span className="flex items-center justify-end gap-1 text-sm text-gray-500">
-      {`읽는데 ${readingTime}분`}
+      {`읽는 데 ${readingTime}분`}
     </span>
   );
 };
