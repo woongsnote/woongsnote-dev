620700e0f47316794c7fc79ea058b1b9418760c8
woongsnote <woongsnote@gmail.com>
2023-11-09 22:41:04 +0900

    fix: update comments

--- src/components/react/Comments.tsx
+++ src/components/react/Comments.tsx
@@ -1,6 +1,8 @@
 import Giscus from '@giscus/react';
 import { useState, useEffect } from 'react';
 
+const id = 'inject-comments';
+
 const Comments = () => {
   const [mounted, setMounted] = useState<boolean>(false);
   useEffect(() => {
@@ -8,21 +10,25 @@ const Comments = () => {
   }, []);
 
   return (
-    <Giscus
-      id="comments"
-      repo="woongsnote/woongsnote-dev"
-      repoId="R_kgDOIZVQXA"
-      category="Comments"
-      categoryId="DIC_kwDOIZVQXM4Caihi"
-      mapping="pathname"
-      strict="0"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="top"
-      theme="dark_dimmed"
-      lang="ko"
-      loading="lazy"
-    />
+    <div id={id}>
+      {mounted ? (
+        <Giscus
+          id="comments"
+          repo="woongsnote/woongsnote-dev"
+          repoId="R_kgDOIZVQXA"
+          category="Comments"
+          categoryId="DIC_kwDOIZVQXM4Caihi"
+          mapping="pathname"
+          strict="0"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme="dark_dimmed"
+          lang="ko"
+          loading="lazy"
+        />
+      ) : null}
+    </div>
   );
 };
 
