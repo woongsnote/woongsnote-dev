2530725f101062b12099f28a694a440f4d28988a
woongsnote <woongsnote@gmail.com>
2023-11-28 12:05:24 +0900

    chore: config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,38 @@ import mdx from '@astrojs/mdx';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
-
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://woongsnote.dev',
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
-  integrations: [tailwind(), mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'dracula',
-      wrap: true
-    }
-  }), sitemap(), react()],
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'dracula',
+        wrap: true,
+      },
+    }),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+    react(),
+  ],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+    imageService: true,
+  }),
 });
