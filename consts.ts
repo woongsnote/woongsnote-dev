98a5d22aa58eebf1c780ae60f09e833d1aaa9a19
woongsnote <woongsnote@gmail.com>
2023-12-02 15:57:18 +0900

    style: UI update

--- consts.ts
+++ consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Woongsnote';
-export const SITE_DESCRIPTION = 'Welcome to my blog!';
+export const SITE_TITLE = 'woongsnote.dev';
+export const SITE_DESCRIPTION = '개발 관련 학습한 지식과 프로젝트들을 기록하기 위한 공간입니다.';
