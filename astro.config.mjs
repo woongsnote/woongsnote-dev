210ac260d52513bfee1ef11dd46effa272a0f327
woongsnote <woongsnote@gmail.com>
2023-12-14 00:52:12 +0900

    feat: add storyblok components

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,35 @@ import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import rehypePrettyCode from 'rehype-pretty-code';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
-// https://astro.build/config
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
 export default defineConfig({
   site: 'https://woongsnote.dev',
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
   integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        teaser: 'storyblok/Teaser',
+        grid: 'storyblok/Grid',
+        section: 'storyblok/Section',
+        'latest-articles': 'storyblok/LatestArticles',
+        'all-articles': 'storyblok/AllArticles',
+        article: 'storyblok/Article',
+        hero: 'storyblok/Hero',
+      },
+      apiOptions: {
+        region: 'us',
+      },
+    }),
     tailwind(),
     mdx({
       syntaxHighlight: false,
@@ -42,6 +63,11 @@ export default defineConfig({
       enabled: true,
     },
     imageService: true,
-    devImageService: 'sharp',
   }),
+  vite: {
+    plugins: [mkcert()],
+    server: {
+      https: true,
+    },
+  },
 });
