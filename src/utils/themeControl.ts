5f1c5877b0bceccca492d7f0c01e7647442e0e5e
woongsnote <woongsnote@gmail.com>
2023-12-14 00:51:10 +0900

    refactor: remove unused files

--- src/utils/themeControl.ts
+++
@@ -1,9 +0,0 @@
-export const getCurrentTheme = (): string => {
-  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-    return localStorage.getItem('theme')!;
-  }
-  if (window.matchMedia('(prefers-color-schema: dark)').matches) {
-    return 'dark';
-  }
-  return 'light';
-};
