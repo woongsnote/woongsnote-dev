a7152ecb1d1146bddaa42e9cfc7157c7cf243b02
woongsnote <woongsnote@gmail.com>
2023-12-18 00:08:05 +0900

    fix: components update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,6 @@ import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import rehypePrettyCode from 'rehype-pretty-code';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
-
-const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
   site: 'https://woongsnote.dev',
@@ -18,23 +13,6 @@ export default defineConfig({
     remarkPlugins: [remarkReadingTime],
   },
   integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        page: 'storyblok/Page',
-        feature: 'storyblok/Feature',
-        teaser: 'storyblok/Teaser',
-        grid: 'storyblok/Grid',
-        section: 'storyblok/Section',
-        'latest-articles': 'storyblok/LatestArticles',
-        'all-articles': 'storyblok/AllArticles',
-        article: 'storyblok/Article',
-        hero: 'storyblok/Hero',
-      },
-      apiOptions: {
-        region: 'us',
-      },
-    }),
     tailwind(),
     mdx({
       syntaxHighlight: false,
@@ -64,10 +42,4 @@ export default defineConfig({
     },
     imageService: true,
   }),
-  vite: {
-    plugins: [mkcert()],
-    server: {
-      https: true,
-    },
-  },
 });
