69186b87c1bdd7803fd68e0fbe8d3eb13f04fa75
woongsnote <woongsnote@gmail.com>
2023-12-25 23:22:14 +0900

    refactor: update pages & components

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,31 @@
 import { defineCollection, z } from 'astro:content';
 
+// Define the blog collection
 const blog = defineCollection({
   type: 'content',
   schema: z.object({
+    // Title of the blog post
     title: z.string(),
+
+    // Description of the blog post
     description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    categories: z.array(z.string()).default(['others']),
-    tags: z.array(z.string()).default(['others']),
+
+    // Publication date of the blog post
+    publishedDate: z.string().transform((str) => new Date(str)),
+
+    // URL of the hero image for the blog post
+    image: z.object({
+      url: z.string(), // Source URL of the image
+      alt: z.string(), // Alternative text for the image
+    }),
+
+    // category that the blog post belongs to, defaulting to 'others'
+    category: z.string(),
+
+    // Tags associated with the blog post
+    tags: z.array(z.string()),
+
+    // Author of the blog post
     author: z.string(),
   }),
 });
