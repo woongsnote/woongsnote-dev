23dc6f0e055428f41cde5a60d5514ca26779013a
woongsnote <woongsnote@gmail.com>
2024-01-17 15:41:01 +0900

    fix: update Components

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,39 @@
+import { nullable } from 'astro/zod';
 import { defineCollection, z } from 'astro:content';
 
 // Define the blog collection
 const blog = defineCollection({
   type: 'content',
-  schema: z.object({
-    // Title of the blog post
-    title: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      // Title of the blog post
+      title: z.string(),
 
-    // Description of the blog post
-    description: z.string(),
+      // Description of the blog post
+      description: z.string(),
 
-    // Publication date of the blog post
-    publishedDate: z.string().transform((str) => new Date(str)),
+      // Publication date of the blog post
+      publishedDate: z.string().transform((str) => new Date(str)),
 
-    // URL of the image for the blog post
-    image: z.object({
-      url: z.string(), // Source URL of the image
-      alt: z.string(), // Alternative text for the image
+      // URL of the image for the blog post
+      image: z.object({
+        url: z.string(), // Source URL of the image
+        alt: z.string(), // Alternative text for the image
+      }).optional(),
+
+      cover: image().optional(),
+
+      coverAlt: z.string().optional(),
+
+      // category that the blog post belongs
+      category: z.string(),
+
+      // Tags associated with the blog post
+      tags: z.array(z.string()),
+
+      // Author of the blog post
+      author: z.string(),
     }),
-
-    // category that the blog post belongs
-    category: z.string(),
-
-    // Tags associated with the blog post
-    tags: z.array(z.string()),
-
-    // Author of the blog post
-    author: z.string(),
-  }),
 });
 
 export const collections = { blog };
