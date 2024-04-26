5ad4ba0150ada8f9b8f62d94b1ad83af9a74144d
woongsnote <woongsnote@gmail.com>
2024-04-26 11:31:25 +0900

    update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import config from '@config/site-config';
+import { SITE as config } from '@config/site-config';
 
 export async function GET(context) {
   const blog = await getCollection('blog');
