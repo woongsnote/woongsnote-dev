6101f6d1dceece8b8f1cf34fd037fd629a35fc8b
woongsnote <woongsnote@gmail.com>
2024-06-10 13:57:42 +0900

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,37 +5,42 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import rehypePrettyCode from 'rehype-pretty-code';
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 const prettyCodeOptions = {
   theme: {
     dark: 'one-dark-pro',
-    light: 'github-light'
-  }
+    light: 'github-light',
+  },
 };
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.woongsnote.dev',
-  integrations: [tailwind(), mdx({
-    syntaxHighlight: false,
-    remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]]
-  }), sitemap({
-    changefreq: 'daily',
-    priority: 1.0,
-    lastmod: new Date()
-  }), icon()],
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: false,
+      remarkPlugins: [remarkReadingTime],
+      rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+    }),
+    sitemap({
+      changefreq: 'daily',
+      priority: 1.0,
+      lastmod: new Date(),
+    }),
+    icon(),
+  ],
   output: 'server',
   adapter: vercel({
     isr: {
-      expiration: 60 * 60 * 24
+      expiration: 60 * 60 * 24,
     },
     webAnalytics: {
-      enabled: true
-    }
+      enabled: true,
+    },
   }),
   security: {
-    checkOrigin: true
-  }
+    checkOrigin: true,
+  },
 });
