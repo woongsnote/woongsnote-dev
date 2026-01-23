f564bcb158175aece6398a64898471d7e7bc695c
Jiwoong Moon <woongsnote@gmail.com>
2026-01-24 00:35:55 +0900

    update [slug].astro and rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,8 +5,8 @@ import { SITE as config } from '@config/site-config';
 export async function GET(context) {
   const blog = await getCollection('blog');
   return rss({
-    title: config.metadata.meta_title,
-    description: config.metadata.meta_description,
+    title: config.metadata.title,
+    description: config.metadata.description,
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
