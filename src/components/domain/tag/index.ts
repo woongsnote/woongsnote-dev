754864202a2593626c30f1847d5461027a0ba70e
Jiwoong Moon <woongsnote@gmail.com>
2026-01-30 01:04:16 +0900

    update 11 files, delete 1 file, move 29 files and create 14 files delete 155 files

---
+++ src/components/domain/tag/index.ts
@@ -0,0 +1,2 @@
+export { default as Tag } from './Tag.astro';
+export { default as TagList } from './TagList.astro';
--- src/config/pages.ts
+++ src/config/pages.ts
@@ -1,23 +1,15 @@
-export const PAGE_META = {
+import type { HeadProps } from '@layouts/head';
+
+export type PageKey = 'home' | 'about';
+
+export const PAGE_META: Record<PageKey, HeadProps> = {
   home: {
-    title: 'Home',
+    title: 'Woongsnote',
     description: '문지웅의 기술 블로그',
+    canonicalUrl: '/',
   },
   about: {
     title: 'About',
     description: '저를 소개합니다.',
   },
-  404: {
-    title: '404 Error',
-    description: '페이지를 찾을 수 없습니다.',
-  },
 } as const;
-
-/**
- * PAGE_META의 key 타입
- * - navigation
- * - layout
- * - SEO
- * 에서 공통으로 사용
- */
-export type PageKey = keyof typeof PAGE_META;
