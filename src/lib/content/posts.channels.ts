1570d52d0cf3f6d76282beb73751393406552e54
Jiwoong Moon <woongsnote@gmail.com>
2026-03-14 10:48:41 +0900

    update 9 files, delete 2 files and create 3 files

--- src/lib/content/posts.channels.ts
+++ src/lib/content/posts.channels.ts
@@ -1,10 +1,9 @@
 import { getPosts } from './posts.query';
 
-export const HOME_POST_COUNT = 9;
-export const RSS_POST_COUNT = 20;
+export const HOME_POST_COUNT = 10;
 
 export const getHomePosts = () => getPosts(HOME_POST_COUNT);
 export const getArchivePosts = () => getPosts();
-export const getRssPosts = () => getPosts(RSS_POST_COUNT);
+export const getRssPosts = () => getPosts();
 export const getSitemapPosts = () => getPosts();
 export const getSearchPosts = () => getPosts();
