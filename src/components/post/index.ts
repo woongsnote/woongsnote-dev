f4c14b28f72c713f1a5621276a853dfe95f4e750
Jiwoong Moon <woongsnote@gmail.com>
2026-04-06 06:11:17 +0900

    Merge branch 'dev' of https://github.com/woongsnote/woongsnote-dev into dev (#323)

--- src/components/post/index.ts
+++ src/components/post/index.ts
@@ -3,3 +3,4 @@ export { default as ReadingTimeText } from './ReadingTimeText.astro';
 export { default as Comments } from './PostComments.astro';
 export { default as ShareButton } from './PostShareButton.astro';
 export { default as Toc } from './Toc.astro';
+export { default as TagList } from './TagList.astro';
