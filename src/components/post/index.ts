12ed3eda065bd395f8efffa1f26f111db305aed4
Jiwoong Moon <woongsnote@gmail.com>
2026-05-07 21:03:13 +0900

    update 9 files

--- src/components/post/index.ts
+++ src/components/post/index.ts
@@ -4,3 +4,4 @@ export { default as Comments } from './PostComments.astro';
 export { default as ShareButton } from './PostShareButton.astro';
 export { default as Toc } from './Toc.astro';
 export { default as TagList } from './TagList.astro';
+export { default as PostItem } from './PostItem.astro';
